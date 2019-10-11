75c9187ec49783c275e3ba82b16c59a883b185d8
Jeffrey Mann <jwmann22@gmail.com>
2019-10-10 22:00:41 -0400

    changed base ref for firebase data in patient-list.js

--- assets/javascript/patient-list.js
+++ assets/javascript/patient-list.js
@@ -1,20 +1,3 @@
-// Initialize Firebase (YOUR OWN APP)
-var config = {
-  apiKey: "AIzaSyAARBRklYvGPYYT7lRMiFDolSgRivtuq3s",
-  authDomain: "gt-project-1-group-11-b08d3.firebaseapp.com",
-  databaseURL: "https://gt-project-1-group-11-b08d3.firebaseio.com",
-  projectId: "gt-project-1-group-11-b08d3",
-  storageBucket: "gt-project-1-group-11-b08d3.appspot.com",
-  messagingSenderId: "986153651299",
-  appId: "1:986153651299:web:560e09006a98a2ceecbc56",
-  measurementId: "G-LC5DNRC73Q"
-};
-
-firebase.initializeApp(config);
-
-// Variable to reference the database
-var database = firebase.database();
-
 var patientName;
 var patientAge;
 var patientFitbit;
@@ -52,11 +35,11 @@ $("#add-btn").on("click", function(event) {
       date: lastRecordedSession
     }
   };
-  database.ref().push(patientObject);
+  PATIENTS.push(patientObject);
 });
 
 // Firebase watcher .on("child_added")
-database.ref().on(
+PATIENTS.on(
   "child_added",
   function(snapshot) {
     // storing the snapshot.val() in a variable for convenience
@@ -71,7 +54,7 @@ database.ref().on(
   }
 );
 
-database.ref().on(
+PATIENTS.on(
   "child_removed",
   function(snapshot) {
     removePatientRow(snapshot.ref.key);
