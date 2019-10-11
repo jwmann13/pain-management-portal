d129d3457ea5254ead0e0b297d9d3d9e3fa78901
Chase Laptop <james.chase.richards@gmail.com>
2019-10-10 20:28:53 -0400

    style changes

--- assets/javascript/patient-list.js
+++ assets/javascript/patient-list.js
@@ -1,13 +1,13 @@
 // Initialize Firebase (YOUR OWN APP)
 var config = {
-  apiKey: "AIzaSyATH9AkcU8c8vKGeVuakMFlyPe_CSil0kk",
-  authDomain: "gt-project-group-11.firebaseapp.com",
-  databaseURL: "https://gt-project-group-11.firebaseio.com",
-  projectId: "gt-project-group-11",
-  storageBucket: "gt-project-group-11.appspot.com",
-  messagingSenderId: "916608204316",
-  appId: "1:916608204316:web:9b8d38bc3ad3eafa1729d9",
-  measurementId: "G-LQK0SJKC5D"
+  apiKey: "AIzaSyAARBRklYvGPYYT7lRMiFDolSgRivtuq3s",
+  authDomain: "gt-project-1-group-11-b08d3.firebaseapp.com",
+  databaseURL: "https://gt-project-1-group-11-b08d3.firebaseio.com",
+  projectId: "gt-project-1-group-11-b08d3",
+  storageBucket: "gt-project-1-group-11-b08d3.appspot.com",
+  messagingSenderId: "986153651299",
+  appId: "1:986153651299:web:560e09006a98a2ceecbc56",
+  measurementId: "G-LC5DNRC73Q"
 };
 
 firebase.initializeApp(config);
@@ -86,8 +86,8 @@ database.ref().on(
 function addPatientRow(element, uniqueID) {
   const tableBody = document.getElementById("tableData");
   let dataHtml = `<tr id=${uniqueID}>
-          <td>${element.patient}</td>
-          <td>${element.sessions.date}</td>
+            <td><a href="provider-patient-info.html">${element.patient}</a></td>
+            <td><a href="provider-patient-info.html">${element.sessions.date}</a></td>
           <td><a onclick="removePatientFromDatabase('${uniqueID}');">X</a></td>
         </tr>`;
   tableBody.innerHTML += dataHtml;
