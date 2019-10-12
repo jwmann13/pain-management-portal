f88d1f4cf9eeffadfe3dd0b60eba9ba8ddf8e768
Cesar Sanchez-Vegas <cesar.sanchezvegas82@gmail.com>
2019-10-12 13:18:22 -0400

    fitbit api working

---
+++ assets/javascript/FitbitAPI.js
@@ -0,0 +1,34 @@
+// get the url 
+var url = window.location.href;
+//getting the access token from url 
+var access_token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkI2MlQiLCJzdWIiOiI3VDIzMjkiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd3dlaSB3c29jIHdhY3Qgd3NldCB3bG9jIiwiZXhwIjoxNTcxNDE5MjE2LCJpYXQiOjE1NzA4OTA4MTZ9.mubIeZA6bdh6Q834LH84enZEQYvCOHYNDYCrkzWtqIw';
+
+// var access_token = url.split("#")[1].split("=")[1].split("&")[0]; 
+// get the userid 
+var userId = '7T2329';
+
+// var userId = url.split("#")[1].split("=")[2].split("&")[0]; 
+console.log(access_token);
+console.log(userId);
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://api.fitbit.com/1/user/' + userId + '/activities/heart/date/today/1w.json');
+xhr.setRequestHeader("Authorization", 'Bearer ' + access_token);
+xhr.onload = function () {
+    if (xhr.status === 200) {
+        // JSON.stringify(xhr.responseText)
+        console.log(xhr.responseText)
+        // created a new variable from the response object
+        let response = xhr.responseText
+        // gradded the targeted div 
+        let target = $(".patient-home-pain") 
+        // created new p tag 
+        let p  = $('<p>')
+        // appended the response data to the p tag
+        p.append(response)
+        // appended the p to the html
+        target.append(p)
+
+    }
+};
+xhr.send()
