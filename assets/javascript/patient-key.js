2edb0921a2d50be60c09f2ae7bd83716294fce9f
Jeffrey Mann <jwmann22@gmail.com>
2019-10-12 11:20:46 -0400

    failing to get patient key into patient survey data

--- assets/javascript/patient-key.js
+++ assets/javascript/patient-key.js
@@ -1,16 +1,20 @@
 let patientKey;
 
-$('#patient-home-btn').on('click', function(event) {
-    event.preventDefault();
+$('#patient-home-btn').on('click', function (event) {
+  event.preventDefault();
 
-    let email = $('#patient-home-email').val().trim();
+  let email = $('#patient-home-email').val().trim();
 
-    patientKey = authenticatePatientByEmail(email);
+  PATIENTS.orderByChild('email').equalTo(email).on('value', function (snapshot) {
+    patientKey = Object.keys(snapshot.val())[0];
+    console.log(snapshot.val());
+    console.log(patientKey);
+  });
 });
 
 function authenticatePatientByEmail(email) {
 
-  PATIENTS.orderByChild('email').equalTo(email).on('value', function(snapshot) {
+  PATIENTS.orderByChild('email').equalTo(email).on('value', function (snapshot) {
     return Object.keys(snapshot.val())[0];
   });
 }
