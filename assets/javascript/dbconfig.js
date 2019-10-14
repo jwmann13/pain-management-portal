4c30a5a9b2432407b68d6adc2e29e92c92741789
Chase Laptop <james.chase.richards@gmail.com>
2019-10-14 18:45:56 -0400

    style changes and api fixes

--- assets/javascript/dbconfig.js
+++ assets/javascript/dbconfig.js
@@ -1,17 +1,17 @@
 var config = {
-    apiKey: "AIzaSyAARBRklYvGPYYT7lRMiFDolSgRivtuq3s",
-    authDomain: "gt-project-1-group-11-b08d3.firebaseapp.com",
-    databaseURL: "https://gt-project-1-group-11-b08d3.firebaseio.com",
-    projectId: "gt-project-1-group-11-b08d3",
-    storageBucket: "gt-project-1-group-11-b08d3.appspot.com",
-    messagingSenderId: "986153651299",
-    appId: "1:986153651299:web:560e09006a98a2ceecbc56",
-    measurementId: "G-LC5DNRC73Q"
-  };
+  apiKey: "AIzaSyAARBRklYvGPYYT7lRMiFDolSgRivtuq3s",
+  authDomain: "gt-project-1-group-11-b08d3.firebaseapp.com",
+  databaseURL: "https://gt-project-1-group-11-b08d3.firebaseio.com",
+  projectId: "gt-project-1-group-11-b08d3",
+  storageBucket: "gt-project-1-group-11-b08d3.appspot.com",
+  messagingSenderId: "986153651299",
+  appId: "1:986153651299:web:560e09006a98a2ceecbc56",
+  measurementId: "G-LC5DNRC73Q"
+};
 
 firebase.initializeApp(config);
 
 var database = firebase.database();
 
-const PATIENTS = database.ref('patients');
-const SESSIONS = database.ref('sessions');
+const PATIENTS = database.ref("patients");
+const SESSIONS = database.ref("sessions");
