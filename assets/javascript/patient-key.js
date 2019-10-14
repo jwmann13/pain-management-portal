ea6e7a7bf44bbc526b8c916db642712667ad8f6d
Jeffrey Mann <jwmann22@gmail.com>
2019-10-14 14:06:50 -0400

    have patient-auth.js adding sessions with patient ID and date when users authenticate and adding key of the new session to patients sessionKeys property

--- assets/javascript/patient-key.js
+++ assets/javascript/patient-key.js
@@ -2,26 +2,31 @@ $('#patient-auth-btn').on('click', function (event) {
   event.preventDefault();
 
   let email = $('#patient-auth-email').val().trim();
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, "0");
-  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-  var yyyy = today.getFullYear();
-  today = mm + "/" + dd + "/" + yyyy;
 
-  PATIENTS.orderByChild('email').equalTo(email).on('value', function (snapshot) {
+  PATIENTS.orderByChild('email').equalTo(email).once('value', function (snapshot) {
     let patientKey = Object.keys(snapshot.val())[0];
 
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, "0");
+    var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+    var yyyy = today.getFullYear();
+    today = mm + "/" + dd + "/" + yyyy;
 
     let sessionInfo = {
       patient: patientKey,
-      date: today
-    }
-    SESSIONS.push(sessionInfo);
+      date: today,
+      surveyTaken: false
+    };
 
-    let pastSessions = PATIENTS.child(patientKey + '/sessions')
-    PATIENTS.child(patientKey).update({
-      sessionsKeys: pastSessions
+    SESSIONS.push(sessionInfo).once('value', function (sessSnap) {
+      console.log(sessSnap.key)
+      PATIENTS.child(patientKey).update({
+        sessionKeys: sessSnap.key
+      })
     });
+
+    // let pastSessions = PATIENTS.child(patientKey + '/sessions')
+
   });
 });
 
